01dd2356cb3e1a1cd92813bb9606d9d43af01b4f
Brandon Rhodes <brandon@rhodesmill.org>
2020-04-17 17:35:26 -0400

    Render the Canyon polygons, though barely visible

--- build/test.py
+++ build/test.py
@@ -9,6 +9,8 @@ import mapnik
 m = mapnik.Map(610,300)
 m.background = mapnik.Color('steelblue')
 
+#print(dir(mapnik))
+
 s = mapnik.Style() # style object to hold rules
 r = mapnik.Rule() # rule object to hold symbolizers
 # to fill a polygon we create a PolygonSymbolizer
@@ -19,7 +21,7 @@ r.symbols.append(polygon_symbolizer) # add the symbolizer to the rule object
 # to add outlines to a polygon we create a LineSymbolizer
 line_symbolizer = mapnik.LineSymbolizer()
 line_symbolizer.stroke = mapnik.Color('rgb(50%,50%,50%)')
-line_symbolizer.stroke_width = 0.1
+line_symbolizer.stroke_width = 0.0 #1.0  #0.1
 r.symbols.append(line_symbolizer) # add the symbolizer to the rule object
 s.rules.append(r) # now add the rule to the style and we're done
 
@@ -31,15 +33,26 @@ layer = mapnik.Layer('world') # new layer called 'world' (we could name it anyth
 # note: layer.srs will default to '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
 
 layer.datasource = ds
-
 layer.styles.append('My Style')
+#m.layers.append(layer)
 
+# extent = mapnik.Box2d(-130.0, 30.0, -100.0, 45.0)
+# m.zoom_to_box(extent)
+
+#mapnik.Ogr(file='test_point_line.gpx')
+
+layer = mapnik.Layer('Geolines')
+geolines_datasource = mapnik.Ogr(
+    #file='geolines.geojson',
+    #layer='geolines',
+    file='geopolys.geojson',
+    layer='geopolys',
+)  # does this add a datasource?
+layer.datasource = geolines_datasource
+layer.styles.append('My Style')
 m.layers.append(layer)
-#m.zoom_all()
 
-extent = mapnik.Box2d(-130.0, 30.0, -100.0, 45.0)
-m.zoom_to_box(extent)
-
+m.zoom_all()
 mapnik.render_to_file(m,'world.png', 'png')
 
 
